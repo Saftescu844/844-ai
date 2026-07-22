eec8f4fb59ef09c5d9daa4efa6984f8831ddbb10
Gabriel <46198870+Saftescu844@users.noreply.github.com>
2026-07-22 19:13:10 +0000

    Elimin endpoint debug temporar

--- src/app/debug-dbhash/route.ts
+++
@@ -1,7 +0,0 @@
-import crypto from 'crypto'
-
-export async function GET() {
-  const url = process.env.DATABASE_URL || ''
-  const hash = crypto.createHash('sha256').update(url).digest('hex')
-  return new Response(hash)
-}
